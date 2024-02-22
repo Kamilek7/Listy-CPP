8011e8d769e9f91adbb8faeb8ae4f619dcb910df
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-02-22 11:25:06 +0100

    zadanie 2

--- lista1.cpp
+++ lista1.cpp
@@ -1,35 +1,60 @@
-#include <iostream>
-#include <cmath>
-double pierwsze(int x)
-{
-    return pow(-1,x+1)/(2*x-1);
-}
-double drugie(int x)
-{
-    return 4*pow(x,2)/(4*pow(x,2)-1);
-}
-double trzecie(int x)
-{
-    return 1/pow(2*x-1,2);
-}
+// ZAD 1
 
-double sum(int start, int n, double (*func)(int))
-{
-    double suma = 0;
-    for (int i = start; i<=n;i++)
-        suma += func(i);
-    return suma;
-}
-double product(int start, int n, double (*func)(int))
-{
-    double suma = 1;
-    for (int i = start; i<=n;i++)
-        suma *= func(i);
-    return suma;
-}
+// #include <iostream>
+// #include <cmath>
+// double pierwsze(int x)
+// {
+//     return pow(-1,x+1)/(2*x-1);
+// }
+// double drugie(int x)
+// {
+//     return 4*pow(x,2)/(4*pow(x,2)-1);
+// }
+// double trzecie(int x)
+// {
+//     return 1/pow(2*x-1,2);
+// }
 
-int main()
-{
-    std::cout<<4*sum(1,1000000, &pierwsze)<< std::endl <<2*product(1,1000000, &drugie) << std::endl <<pow(8*sum(1,1000000, &trzecie),0.5);
-    return 0;
-}
+// double sum(int start, int n, double (*func)(int))
+// {
+//     double suma = 0;
+//     for (int i = start; i<=n;i++)
+//         suma += func(i);
+//     return suma;
+// }
+// double product(int start, int n, double (*func)(int))
+// {
+//     double suma = 1;
+//     for (int i = start; i<=n;i++)
+//         suma *= func(i);
+//     return suma;
+// }
+
+// int main()
+// {
+//     std::cout<<4*sum(1,1000000, &pierwsze)<< std::endl <<2*product(1,1000000, &drugie) << std::endl <<pow(8*sum(1,1000000, &trzecie),0.5);
+//     return 0;
+// }
+
+// ZAD 2
+
+// #include <iostream>
+// #include <cmath>
+// double archimedes(int n)
+// {
+//     if (n == 0)
+//     {
+//         return (1/pow(3,0.5));
+//     }
+//     else
+//     {
+//         double temp = archimedes(n-1);
+//         return (pow(temp*temp+1,0.5)-1)/temp;
+//     }
+// }
+// int main()
+// {
+//     for (int i=0; i<=30; i++)
+//         std::cout << 6* pow(2,i) * archimedes(i) << std::endl;
+//     return 0;
+// }
