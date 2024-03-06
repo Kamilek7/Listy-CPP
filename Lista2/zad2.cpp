5bcd58eee39d71a1a2514c2eb6118af7033d72fa
kamilek7 <85680654+Kamilek7@users.noreply.github.com>
2024-03-06 20:01:39 +0100

    lista 2 zadanie 2

--- Lista2/zad2.cpp
+++ Lista2/zad2.cpp
@@ -1,15 +1,27 @@
 // ZAD 2
 
 #include <iostream>
-#include <cstdlib>
-
-void sortuj(int* tab, size_t size)
+#include <stdlib.h>
+int liczby[] = { 1, 4, 7, 2, 5, 9, 2, 8 };
+int sortuj(const void* a, const void* b)
 {
-
+    int temp = *(int*)a;
+    int temp1 = *(int*)b;
+    if (temp % 2 == temp1 % 2) 
+        if (temp % 2) 
+            return temp1 - temp; 
+        else
+            return temp - temp1;
+    else 
+        return temp % 2;
 }
-
 int main()
 {
-
+    for (int i=0; i<8; i++)
+        std::cout << liczby[i] << " ";
+    std::cout<<std::endl;
+    qsort (liczby, 8, sizeof(int), sortuj);
+    for (int i=0; i<8; i++)
+        std::cout << liczby[i] << " ";
     return 0;
 }
